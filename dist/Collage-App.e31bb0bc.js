dba5de84ae8ad764540667c3ddca2aad2dfdb9ec
岳涵黃 <yuehanhuang@yuehanhuangde-MacBook-Pro.local>
2019-11-26 18:24:33 +0900

    Update CSS

--- dist/Collage-App.e31bb0bc.js
+++ dist/Collage-App.e31bb0bc.js
@@ -66610,7 +66610,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59540" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62052" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
